6c3ec2210581cf5d00b6904c3b008d5c959034f9
Vanille <beernaert.v@gmail.com>
2020-09-20 11:35:48 +0200

    v.1.0.1
    Update javascript and appearance

--- js/script.js
+++ js/script.js
@@ -10,21 +10,23 @@ $('#todo').append('<form class="form-signin" id="registrationForm"> </div>');
 $('#registrationForm').append('<div class="form-label-group" id="test"> </div>');
 
 
-$('#test').append('<input type="texte" id="addtask" class="form-control" placeholder="Press ↵ Enter to validate" autocomplete="off" autofocus>' + '<label for="inputEmail">Press ↵ Enter to validate</label>');
+$('#test').append('<input type="texte" id="addtask" class="form-control" placeholder="Press ↵ Enter to validate" autocomplete="off" autofocus>');
 
 
 
 /*
  * Listening submit of form id="registrationForm"
  */
-$('#registrationForm').submit(function(e) {
+$('#registrationForm').submit(function (e) {
+  e.preventDefault(); // The browser cannot send the form
 
   if ($('#addtask').val() == '') {
+    // Hidden warning alert message
+    $('body').append('<div class="warning-message alert alert-dismissible fade hide text-center" role="alert">' + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' + '<span aria-hidden="true">' + '&times;' +
+      '</span>' + '</button>' + 'Warning ! Please enter at least 1 character. ' + '</div>');
+    $('.hide').toggleClass('show').delay(3000).fadeOut('slow'); // show alert message
 
-    $('.small-title').prepend('<div class="alert alert-danger alert-dismissible fade show text-center" role="alert">' + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' + '<span aria-hidden="true">' + '&times;' +
-      '</span>' + '</button>' + 'Warning ! Empty task, please enter at least 1 character. ' + '</div>'); // warning message
-
-    console.log('Error => Empty input'); // info in console
+    console.log('Error => Empty input');
 
   } else {
     e.preventDefault(); // The browser cannot send the form
@@ -34,34 +36,43 @@ $('#registrationForm').submit(function(e) {
       type: 'post',
       dataType: 'html',
 
-      success: function(response) {
+      success: function (response) {
 
         /*
          * For each  // to show each new checkbox
          */
-        $('#tasks').each(function() {
+        $('#tasks').each(function () {
+
           $(this).prepend('<div class="newtask input-group mb-3 checkbox">' + '<div class="input-group-prepend">' + ' <div class="input-group-text">' +
             '<input type="checkbox" aria-label="Checkbox for following text input" >' + ' </div>' + '</div>' +
             '<input type="text" class="test form-control"' + 'value="' + $('#addtask').val() + '">' + '</div>');
-
-          $('.newtask').addClass('text-secondary'); // the 'newtask' class which has just been created above
-          $('#registrationForm')[0].reset();
+          console.log('New task => Create');
+          $('#registrationForm')[0].reset(); // clean the Form input
         });
+
+        // Start Sortable.js to drag and drop tasks
+        console.log('newtask');
+
+        new Sortable(tasks, {
+          animation: 150,
+          ghostClass: 'blue-background-class'
+        }); // End Sortable.js
 
         /*
          * For each  // to hide the accomplished tasks
          */
-        $('.checkbox').each(function() {
-          $(this).click(function(e) {
-            $(this).hide('newtask');
+        $('.checkbox').each(function () {
+          $(this).click(function (e) {
 
-            console.log('Task => Checked'); // info in console
+            $(this).hide('newtask');
+            console.log('New task => Checked'); // info in console
+
           });
         });
 
 
       }, // end function response
-      error: function() {
+      error: function () {
         alert('error');
       } // end function error
     }); // end ajax
